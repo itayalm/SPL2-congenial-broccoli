9b2eff5bc57486bb1a2d5c0306a62dcc7848ff6f
itayalm <itayalm@post.bgu.ac.il>
2019-12-25 22:13:49 +0200

    in Diary added static field diary_singleton, List<Report> reports and int total. also added funcs getInstance, private constructor.

--- src/main/java/bgu/spl/mics/application/passiveObjects/Agent.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Agent.java
@@ -7,12 +7,14 @@ package bgu.spl.mics.application.passiveObjects;
  * You may add ONLY private fields and methods to this class.
  */
 public class Agent {
-
+	private String serialNumber;
+	private String name;
+	private boolean available;
 	/**
 	 * Sets the serial number of an agent.
 	 */
 	public void setSerialNumber(String serialNumber) {
-		// TODO Implement this
+		this.serialNumber = serialNumber;
 	}
 
 	/**
@@ -21,15 +23,14 @@ public class Agent {
      * @return The serial number of an agent.
      */
 	public String getSerialNumber() {
-		// TODO Implement this
-		return null;
+		return serialNumber;
 	}
 
 	/**
 	 * Sets the name of the agent.
 	 */
 	public void setName(String name) {
-		// TODO Implement this
+		this.name = name;
 	}
 
 	/**
@@ -38,8 +39,7 @@ public class Agent {
      * @return the name of the agent.
      */
 	public String getName() {
-		// TODO Implement this
-		return null;
+		return this.name;
 	}
 
 	/**
@@ -48,21 +48,20 @@ public class Agent {
      * @return if the agent is available.
      */
 	public boolean isAvailable() {
-		// TODO Implement this
-		return false;
+		return available;
 	}
 
 	/**
 	 * Acquires an agent.
 	 */
 	public void acquire(){
-		// TODO Implement this
+		this.available = false;
 	}
 
 	/**
 	 * Releases an agent.
 	 */
 	public void release(){
-		// TODO Implement this
+		this.available = true;
 	}
 }
