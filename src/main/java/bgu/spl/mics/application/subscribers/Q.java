b9fb733fcf14c7965c45a5be0d4d446df12f1904
TommerFed <tommerico2000@gmail.com>
2019-12-26 22:54:40 +0200

    added basic implementation for sub/pub classes but need event implemtation to continue

--- src/main/java/bgu/spl/mics/application/subscribers/Q.java
+++ src/main/java/bgu/spl/mics/application/subscribers/Q.java
@@ -1,6 +1,7 @@
 package bgu.spl.mics.application.subscribers;
 
 import bgu.spl.mics.Subscriber;
+import bgu.spl.mics.application.passiveObjects.Inventory;
 
 /**
  * Q is the only Subscriber\Publisher that has access to the {@link bgu.spl.mics.application.passiveObjects.Inventory}.
@@ -9,14 +10,17 @@ import bgu.spl.mics.Subscriber;
  * You MAY change constructor signatures and even add new public constructors.
  */
 public class Q extends Subscriber {
-
-	public Q() {
-		super("Change_This_Name");
+	private Inventory inven;
+	private GadgetAvailableEvent gadgets;
+	public Q(String name) {
+		super(name);
+		inven = Inventory.getInstance();
 		// TODO Implement this
 	}
 
 	@Override
 	protected void initialize() {
+		//need Event implementation for this
 		// TODO Implement this
 		
 	}
