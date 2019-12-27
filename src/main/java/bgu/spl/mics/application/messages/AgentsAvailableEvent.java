9f337ade0932cac409233606151a540cb5f59fd2
itayalm <itayalm@post.bgu.ac.il>
2019-12-27 14:09:18 +0200

    added AgentsAvailableEvent

---
+++ src/main/java/bgu/spl/mics/application/messages/AgentsAvailableEvent.java
@@ -0,0 +1,29 @@
+package bgu.spl.mics.application.messages;
+
+import java.util.List;
+
+public class AgentsAvailableEvent {
+    private List<String> serials;
+    int duration;
+
+    private AgentsAvailableEvent(List<String> serials, int duration){
+        this.serials = serials;
+        this.duration = duration;
+    }
+
+    public List<String> getSerials() {
+        return serials;
+    }
+
+    public void setSerials(List<String> serials) {
+        this.serials = serials;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
