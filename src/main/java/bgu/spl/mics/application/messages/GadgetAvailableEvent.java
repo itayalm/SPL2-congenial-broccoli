aa57f3ba0452fb3bf86c3c5fcebd7cb2ff4d0c2a
itayalm <itayalm@post.bgu.ac.il>
2019-12-27 14:13:00 +0200

    added GadgetAvailableEvent

--- src/main/java/bgu/spl/mics/application/messages/GadgetAvailableEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/GadgetAvailableEvent.java
@@ -1,4 +1,17 @@
 package bgu.spl.mics.application.messages;
 
 public class GadgetAvailableEvent {
+    private int id;
+
+    public GadgetAvailableEvent(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
