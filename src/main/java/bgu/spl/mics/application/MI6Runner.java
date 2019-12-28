1d6e18624ed6fa5cd0df11dc9b4411e8628e6c33
itayalm <itayalm@post.bgu.ac.il>
2019-12-28 14:22:17 +0200

    made Tommer's changes in TimeService

--- src/main/java/bgu/spl/mics/application/MI6Runner.java
+++ src/main/java/bgu/spl/mics/application/MI6Runner.java
@@ -1,11 +1,49 @@
 package bgu.spl.mics.application;
 
-/** This is the Main class of the application. You should parse the input file, 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/** This is the Main class of the application. You should parse the input file,
  * create the different instances of the objects, and run the system.
  * In the end, you should output serialized objects.
  */
 public class MI6Runner {
     public static void main(String[] args) {
-        // TODO Implement this
+        JSONParser parser = new JSONParser();
+
+        try (Reader reader = new FileReader("input201[3].json")) {
+
+            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+            System.out.println(jsonObject);
+//            String name = (String) jsonObject.get("name");
+//            System.out.println(name);
+//
+//            long age = (Long) jsonObject.get("age");
+//            System.out.println(age);
+
+            List<String> gadgets= new LinkedList<String>;
+            // loop array
+            JSONArray msg = (JSONArray) jsonObject.get("Inventory");
+            Iterator<String> iterator = msg.iterator();
+            while (iterator.hasNext()) {
+                gadgets.add(iterator.next());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 }
